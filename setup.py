ac9f4682713606607be814b076c0189186f9e1c3
oleg-panichev <olegxpanichev@gmail.com>
2018-09-15 12:59:33 +0300

    Minor fixes in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='kinoa',
-      version='0.0.1',
+      version='0.0.2',
       description='Library for experiments logging.',
       long_description=open('README.md').read(),
       url='https://github.com/oleg-panichev/kinoa',
@@ -12,6 +12,10 @@ setup(name='kinoa',
       author='Oleg Panichev',
       author_email='olegxpanichev@gmail.com',
       packages=['kinoa'],
-      install_requires=['numpy', 'matplotlib', 'pandas',
-                        'scipy>=0.14.0', 'scikit-learn>=0.18'],
+      install_requires=[
+          'numpy', 
+          'matplotlib', 
+          'pandas',
+          'scipy>=0.14.0', 
+          'scikit-learn>=0.18'],
       )
